588c882a6ddae0dba69da933cbd36951cc0c7145
Lewis Lockhart <lewis00001@gmail.com>
2020-02-08 12:14:08 -0700

    messed with the db setup for sequalize - status working

--- models/userinfo.js
+++ models/userinfo.js
@@ -1,22 +1,10 @@
-
-// Sequelize (capital) references the standard library
-var Sequelize = require("sequelize");
-// sequelize (lowercase) references my connection to the DB.
-var sequelize = require("../config/connection.js");
-
-
-// Creates a "userinfo" model that matches up with DB
-var Userinfo = sequelize.define("userinfo", {
-    first_name: Sequelize.STRING,
-    last_name: Sequelize.STRING,
-    user_name: Sequelize.STRING,
+module.exports = function(sequelize, Sequelize) {
+  var Userinfo = sequelize.define("userinfo", {
+    firstName: Sequelize.STRING,
+    lastName: Sequelize.STRING,
+    userName: Sequelize.STRING,
     password: Sequelize.STRING,
-    reenter_password: Sequelize.STRING,
     email: Sequelize.STRING
   });
-
-// Syncs with DB
-Userinfo.sync(); 
-
-
-module.exports = Userinfo; 
+  return Userinfo;
+}
