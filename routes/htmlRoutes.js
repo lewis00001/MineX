70b4073c5c9c4233e1e47f57adba95cee736fc92
Dallin Rodgers <rodgers.dallin@gmail.com>
2020-02-11 20:03:54 -0700

    Update htmlRoutes.js
    render index page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,12 +3,7 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.render("index");
   });
 
   // Load example page and pass in an example by id
@@ -16,14 +11,14 @@ module.exports = function(app) {
     db.userInfo
       .findOne({ where: { id: req.params.id } })
       .then(function(dbExample) {
-        res.render("example", dbExample.dataValues);
+        res.render("MineX", dbExample.dataValues);
       });
   });
 
-  // Load main game page
-  app.get("/MineX", function(req, res) {
-    res.render("MineX", { example: { id:3} });
-  });
+  // // Load main game page
+  // app.get("/MineX", function(req, res) {
+  //   res.render("MineX", { example: { id:3} });
+  // });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
