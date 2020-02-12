3316e810f4fb747513fe9f3255686554c79fa1ed
Dallin Rodgers <rodgers.dallin@gmail.com>
2020-02-11 18:58:06 -0700

    Connection info
    New database,
    new sequelize connection

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,10 @@
 //Initiates the connection to mysql
 
-
 // Dependencies
 var Sequelize = require("sequelize");
 
-// mySQL connection for userinfo_table 
-var sequelize = new Sequelize("userinfo_table", "root", "", {
+// mySQL connection for userinfo_table
+var sequelize = new Sequelize("mineX", "root", "root password here", {
   host: "localhost",
   port: 3306,
   dialect: "mysql",
@@ -16,21 +15,5 @@ var sequelize = new Sequelize("userinfo_table", "root", "", {
   }
 });
 
-// mySQL connection for rocket_table 
-var sequelize = new Sequelize("rocket_table", "root", "", {
-    host: "localhost",
-    port: 3306,
-    dialect: "mysql",
-    pool: {
-      max: 5,
-      min: 0,
-      idle: 10000
-    }
-  });
-
-
 // Exports the connection for other files to use
 module.exports = sequelize;
-
-
-
