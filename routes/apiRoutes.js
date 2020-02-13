db66bfc4870cdfcd3b244808599c3ac712982478
DallinRodgers <rodgers.dallin@gmail.com>
2020-02-13 13:16:03 -0700

    Update apiRoutes.js
    added register and login validation,
    check to see if email exists in db,
    validate password with bcriypt,
    encrypt the password for db storage

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,26 +1,74 @@
 var db = require("../models");
-var { registerValidation } = require("../validation");
+var { registerValidation, loginValidation } = require("../validation");
+var bcrypt = require("bcryptjs");
 
 module.exports = function(app) {
-  // app.get("/api/all", function(req, res) {
-  //   db.userInfo.findAll({}).then(function(dbPost) {
-  //     res.json(dbPost);
-  //   });
-  // });
+  app.get("/api/all", function(req, res) {
+    db.userInfo.findAll({}).then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
 
   app.get("/api/find/:email/:password", function(req, res) {
-    db.userInfo
+    var user;
+        // Validate input
+         var requestBody = {
+           email: req.params.email,
+           password: req.params.password
+         }
+        var {error} = loginValidation(requestBody);
+        if(error) {
+          return res.json(error.details[0].message);
+        }
+
+      // Check for valid email
+      db.userInfo
       .findOne({
         where: {
           email: req.params.email,
-          password: req.params.password
         }
       })
       .then(function(dbPost) {
-        if(dbPost !== undefined || dbPost !== null) {
-          res.json(dbPost.dataValues)
+        if(dbPost === null) {
+          return res.json("Email or Password is incorrect");
         };
       });
+
+      db.userInfo
+      .findOne({
+        where: {
+          email: req.params.email,
+        }
+      })
+      .then(function(dbPost) {
+        if(dbPost !== null){
+          user = dbPost.dataValues;
+          checkPassword()
+        }
+      });
+      // Check if password is correct
+      async function checkPassword(){
+        // Hash password
+        var validPassword = await bcrypt.compare(req.params.password, user.password);
+        if(!validPassword) {
+          return res.json("Email or Password is incorrect");
+        }
+        findUser();
+      }
+
+      function findUser(){
+        db.userInfo
+        .findOne({
+          where: {
+            email: req.params.email
+          }
+        })
+        .then(function(dbPost) {
+          if(dbPost !== undefined || dbPost !== null) {
+            res.json(dbPost.dataValues)
+          };
+        });
+      }
   });
 
   // POST route for saving a new post
@@ -41,23 +89,32 @@ module.exports = function(app) {
       }
     })
     .then(function(dbPost) {
-      console.log("dbPost");
-      console.log(dbPost);
       if(dbPost !== null) {
         return res.json("Email already exists");
       };
     });
 
-    // Create new user
-      db.userInfo
-      .create({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        password: req.body.password,
-        email: req.body.email
-      })
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
+    cryptThePassword()
+
+  async function cryptThePassword(){
+    // Hash password
+    var salt = await bcrypt.genSalt(10);
+    var hashPassword = await bcrypt.hash(req.body.password, salt);
+    createUser(hashPassword);
+  }
+
+  function createUser(secretPassword){
+        // Create new user
+        db.userInfo
+        .create({
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          password: secretPassword,
+          email: req.body.email
+        })
+        .then(function(dbPost) {
+          res.json(dbPost);
+        });
+  }
   });
 };
