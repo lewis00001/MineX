63ee6e1c58c178bdb3ff0048eed3c4a0696b0ce5
DallinRodgers <rodgers.dallin@gmail.com>
2020-02-14 10:44:03 -0700

    Update dashboard.js
    updated js file for new class name

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -4,8 +4,8 @@ console.log(currentRocket);
 
 for (var i = 0; i < rockets.length; i++) {
   // element.classList.contains(class);
-  if (rockets[i].classList.contains("current-rocket")) {
-    rockets[i].classList.remove("current-rocket");
+  if (rockets[i].classList.contains("current-option")) {
+    rockets[i].classList.remove("current-option");
   }
 }
-document.getElementById(currentRocket).classList.add("current-rocket");
+document.getElementById(currentRocket).classList.add("current-option");
