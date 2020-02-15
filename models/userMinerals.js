2f88ea1246172ad882b4d413a1d52729756aef95
Lewis Lockhart <lewis00001@gmail.com>
2020-02-14 20:26:15 -0700

    updated ui, added api for minerals,rockets

---
+++ models/userMinerals.js
@@ -0,0 +1,29 @@
+module.exports = function (sequelize, Sequelize) {
+    var UserMinerals = sequelize.define("userMinerals", {
+        id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+          },
+        uranium: Sequelize.INTEGER,
+        neodymium: Sequelize.INTEGER,
+        praseodymium: Sequelize.INTEGER,
+        galium: Sequelize.INTEGER,
+        idium: Sequelize.INTEGER,
+        terbium: Sequelize.INTEGER,
+        silver: Sequelize.INTEGER,
+        hafnium: Sequelize.INTEGER,
+        rhenium: Sequelize.INTEGER,
+        germanium: Sequelize.INTEGER,
+        gold: Sequelize.INTEGER,
+        platinum: Sequelize.INTEGER,
+        palladium: Sequelize.INTEGER,
+        rhodium: Sequelize.INTEGER,
+        diamond: Sequelize.INTEGER,
+        blueDiamond: Sequelize.INTEGER
+    }, {
+        timestamps: false
+    });
+
+    return UserMinerals;
+};
