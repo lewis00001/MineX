74e98042527a61d8ca0119d7018296547230eb27
Lewis Lockhart <lewis00001@gmail.com>
2020-02-16 14:52:40 -0700

    added modal and logic for ore calc

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -71,3 +71,28 @@ setTimeout(function () {
     console.log(minerals);
   })
 }, 1500);
+
+// modal logic
+// get the modal
+var modal = document.getElementById("ore-modal");
+// get the button that opens the modal
+var btn = document.getElementById("mineBtn");
+// get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+}
